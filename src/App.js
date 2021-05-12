4f7f86b080d60fb3dc39827652fd11019278ac5a
Yash Panchal <yash.panchal@theonetechnologies.co.in>
2021-05-12 11:20:47 +0530

    creation of task6 with materialal-ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import Container from '@material-ui/core/Container';
+import LaptopIcon from '@material-ui/icons/Laptop';
+import TabletAndroidIcon from '@material-ui/icons/TabletAndroid';
+import PhoneAndroidIcon from '@material-ui/icons/PhoneAndroid';
 
-function App() {
+
+const App = () => {
+  const [ Count, setCount] = useState(window.innerWidth)
+  useEffect(() => {
+    console.log("Mounted")
+    
+    function windowSizeInfo() {
+      const currentWidth = window.innerWidth
+      setCount(currentWidth);
+    }
+    
+    window.addEventListener("resize", windowSizeInfo)
+
+    return () => {
+      console.log("Unmounted")
+      window.removeEventListener("resize", windowSizeInfo)
+    }  
+
+  }, []);
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container mx="auto">
+        <h2 component="span"color="text.primary">Current Window Width: {Count}</h2>
+        {
+          window.innerWidth >= 1200 &&
+          <h2>Current Device:&nbsp;&nbsp;
+            <span style={{verticalAlign: "middle"}}>
+              <LaptopIcon fontSize="large"/>
+            </span> &nbsp; (Laptop)
+          </h2>
+        }
+        {
+          window.innerWidth >= 768 && window.innerWidth < 1200 &&
+          <h2>Current Device:&nbsp;&nbsp; 
+            <span style={{verticalAlign: "middle"}}>
+              <TabletAndroidIcon fontSize="large"/>
+            </span> &nbsp; (Tablet)
+          </h2>
+        }
+        {
+          window.innerWidth < 768 &&
+          <h2>Current Device:&nbsp;&nbsp; 
+            <span style={{verticalAlign: "middle"}}>
+              <PhoneAndroidIcon fontSize="large"/>
+            </span> &nbsp;(Smart Phone)
+          </h2>
+        }
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
